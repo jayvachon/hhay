00044a2aa42b2a6df62afe1e1096b8a2a889102a
jayvachon <jaylvachon@gmail.com>
2016-06-17 13:59:19 -0400

    hook up cloudinary

--- public/app.js
+++ public/app.js
@@ -1,95 +1,92 @@
-(function() {
+'use strict';
 
-	'use strict';
+var app = angular.module('hhay', [
+	'ngRoute',
+	'cloudinary'
+]);
 
-	var app = angular.module('hhay', [
-		'ngRoute',
-		'cloudinary'
-	]);
+app.config(['$routeProvider', '$httpProvider', '$locationProvider', function ($routeProvider, $httpProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/main.html'
+		})
+		.when('/game', {
+			templateUrl: 'views/game.html',
+			controller: 'GameController',
+			controllerAs: 'gameCtrl'
+		})
+		.when('/userResponse', {
+			templateUrl: 'views/userResponse.html',
+			controller: 'ResponseController',
+			controllerAs: 'responseCtrl'
+		})
+		.when('/404', {
+			templateUrl: 'views/404.html'
+		})
+		.otherwise({ redirect: '/404' })
+}]);
 
-	app.config(['$routeProvider', '$httpProvider', '$locationProvider', function ($routeProvider, $httpProvider, $locationProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: 'views/main.html'
-			})
-			.when('/game', {
-				templateUrl: 'views/game.html',
-				controller: 'GameController',
-				controllerAs: 'gameCtrl'
-			})
-			.when('/userResponse', {
-				templateUrl: 'views/userResponse.html',
-				controller: 'ResponseController',
-				controllerAs: 'responseCtrl'
-			})
-			.when('/404', {
-				templateUrl: 'views/404.html'
-			})
-			.otherwise({ redirect: '/404' })
-	}]);
+app.controller('GameController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 
-	app.controller('GameController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+	var ctrl = this;
 
-		var ctrl = this;
+	this.applyData = function(data) {
+		var gameData = data.gameData;
+		var exchange = gameData.interaction.exchanges[gameData.exchangeIdx];
+		$scope.data = gameData;
+		$scope.exchange = exchange.text;
+		$scope.options = exchange.options;
+	}
 
-		this.applyData = function(data) {
-			var gameData = data.gameData;
-			var exchange = gameData.interaction.exchanges[gameData.exchangeIdx];
-			$scope.data = gameData;
-			$scope.exchange = exchange.text;
-			$scope.options = exchange.options;
-		}
+	$http.get('/api/game-session')
+		.success(function(data) {
+			ctrl.applyData(data);
+		})
+		.error(function(err) {
+			console.log('error: ' + err);
+		});
 
-		$http.get('/api/game-session')
+	this.selectResponse = function(option) {
+		$http.post('/api/select-response', option)
 			.success(function(data) {
-				ctrl.applyData(data);
+				
+				// If this is the beginning of the round, get the user's response
+				if (data.gameData.interactionIdx === 0 && data.gameData.exchangeIdx === 0) {
+					$location.path('/userResponse');
+				} else {
+					ctrl.applyData(data);
+				}
 			})
 			.error(function(err) {
 				console.log('error: ' + err);
 			});
+	}
+}]);
 
-		this.selectResponse = function(option) {
-			$http.post('/api/select-response', option)
-				.success(function(data) {
-					
-					// If this is the beginning of the round, get the user's response
-					if (data.gameData.interactionIdx === 0 && data.gameData.exchangeIdx === 0) {
-						$location.path('/userResponse');
-					} else {
-						ctrl.applyData(data);
-					}
-				})
-				.error(function(err) {
-					console.log('error: ' + err);
-				});
-		}
-	}]);
+app.controller('ResponseController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 
-	app.controller('ResponseController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+	var ctrl = this;
 
-		var ctrl = this;
+	$http.get('/api/query-position')
+		.success(function(data) {
 
-		$http.get('/api/query-position')
+			// Redirect if the player has not completed the round yet (prevent people from skipping to this part by editing the url)
+			var gameData = data.gameData;
+			if (!gameData || gameData.roundIdx === 0 || gameData.interactionIdx > 0 || gameData.exchangeIdx > 0)
+				$location.path('/game');
+		})
+		.error(function(err) {
+			console.log(err);
+		});
+
+	this.submitResponse = function() {
+
+		$http.post('/api/submit-response', ctrl)
 			.success(function(data) {
-
-				// Redirect if the player has not completed the round yet (prevent people from skipping to this part by editing the url)
-				var gameData = data.gameData;
-				if (!gameData || gameData.roundIdx === 0 || gameData.interactionIdx > 0 || gameData.exchangeIdx > 0)
-					$location.path('/game');
+				$location.path('/game');
 			})
 			.error(function(err) {
-				console.log(err);
+				console.log('error: ' + err);
 			});
-
-		this.submitResponse = function() {
-
-			$http.post('/api/submit-response', ctrl)
-				.success(function(data) {
-					$location.path('/game');
-				})
-				.error(function(err) {
-					console.log('error: ' + err);
-				});
-		}
-	}]);
-})();
+	}
+}]);
